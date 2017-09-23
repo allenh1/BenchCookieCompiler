0a0163ff6023d8ffd44db741dd142cfc5a6f37cf
Hunter L. Allen <hunter@openrobotics.org>
2017-09-23 08:23:14 -0500

    Quircks of GCC 5.4.0, I guess?

--- src/scanner.c
+++ src/scanner.c
@@ -54,81 +54,81 @@ struct token * get_next_token(const char ** source)
 	 * tokens from multi char tokens to avoid redudant checks?
 	 *     ~Eric B.
 	 */
-	if (matches_obracket(*source))
+	if (matches_obracket(*source)) {
 		__set_char_tok(source, OBRACKET);
-	else if (matches_cbracket(*source))
+	} else if (matches_cbracket(*source)) {
 		__set_char_tok(source, CBRACKET);
-	else if (matches_obrace(*source))
+	} else if (matches_obrace(*source)) {
 		__set_char_tok(source, OBRACE);
-	else if (matches_cbrace(*source))
+	} else if (matches_cbrace(*source)) {
 		__set_char_tok(source, CBRACE);
-	else if (matches_oparen(*source))
+	} else if (matches_oparen(*source)) {
 		__set_char_tok(source, OPAREN);
-	else if (matches_cparen(*source))
+	} else if (matches_cparen(*source)) {
 		__set_char_tok(source, CPAREN);
-	else if (matches_plus(*source))
+	} else if (matches_plus(*source)) {
 		__set_char_tok(source, PLUS);
-	else if (matches_minus(*source))
+	} else if (matches_minus(*source)) {
 		__set_char_tok(source, MINUS);
-	else if (matches_star(*source))
+	} else if (matches_star(*source)) {
 		__set_char_tok(source, STAR);
-	else if (matches_semicolon(*source))
+	} else if (matches_semicolon(*source)) {
 		__set_char_tok(source, SEMICOLON);
-	else if (matches_underscore(*source))
+	} else if (matches_underscore(*source)) {
 		__set_char_tok(source, UNDERSCORE);
-	else if (matches_bang(*source))
+	} else if (matches_bang(*source)) {
 		__set_char_tok(source, BANG);
-	else if (matches_func(*source))
+	} else if (matches_func(*source)) {
 		__set_nlen_tok(source, 4, FUNC);
-	else if (matches_comma(*source))
+	} else if (matches_comma(*source)) {
 		__set_char_tok(source, COMMA);
-	else if (matches_fslash(*source))
+	} else if (matches_fslash(*source)) {
 		__set_char_tok(source, FSLASH);
-	else if (matches_bslash(*source))
+	} else if (matches_bslash(*source)) {
 		__set_char_tok(source, BSLASH);
-	else if (matches_ampersand(*source))
+	} else if (matches_ampersand(*source)) {
 		__set_char_tok(source, AMPERSAND);
-	else if (matches_pipe(*source))
+	} else if (matches_pipe(*source)) {
 		__set_char_tok(source, PIPE);
-	else if (matches_at(*source))
+	} else if (matches_at(*source)) {
 		__set_char_tok(source, AT);
-	else if (matches_quote(*source))
+	} else if (matches_quote(*source)) {
 		__set_char_tok(source, QUOTE);
-	else if (matches_apostrophe(*source))
+	} else if (matches_apostrophe(*source)) {
 		__set_char_tok(source, APOSTROPHE);
-	else if (matches_capture(*source))
+	} else if (matches_capture(*source)) {
 		__set_nlen_tok(source, 7, CAPTURE);
-	else if (matches_maybe(*source))
+	} else if (matches_maybe(*source)) {
 		__set_nlen_tok(source, 5, MAYBE);
-	else if (matches_int(*source))
+	} else if (matches_int(*source)) {
 		__set_nlen_tok(source, 3, INT);
-	else if (matches_bool(*source))
+	} else if (matches_bool(*source)) {
 		__set_nlen_tok(source, 4, BOOL);
-	else if (matches_char(*source))
+	} else if (matches_char(*source)) {
 		__set_nlen_tok(source, 4, CHAR);
-	else if (matches_double(*source))
+	} else if (matches_double(*source)) {
 		__set_nlen_tok(source, 6, DOUBLE);
-	else if (matches_long(*source))
+	} else if (matches_long(*source)) {
 		__set_nlen_tok(source, 4, LONG);
-	else if (matches_unsigned(*source))
+	} else if (matches_unsigned(*source)) {
 		__set_nlen_tok(source, 8, UNSIGNED);
-	else if (matches_question(*source))
+	} else if (matches_question(*source)) {
 		__set_char_tok(source, QUESTION);
-	else if (matches_and(*source))
+	} else if (matches_and(*source)) {
 		__set_nlen_tok(source, 3, AND);
-	else if (matches_or(*source))
+	} else if (matches_or(*source)) {
 		__set_nlen_tok(source, 2, OR);
-	else if (matches_just(*source))
+	} else if (matches_just(*source)) {
 		__set_nlen_tok(source, 4, JUST);
-	else if (matches_notoken(*source) || (**source) == '\0')
+	} else if (matches_notoken(*source) || (**source) == '\0') {
 		__set_char_tok(source, NOTOKEN);
-	else if ((imgLen = matches_integer(*source, &tmp))) {
+	} else if ((imgLen = matches_integer(*source, &tmp))) {
 		token->image = tmp;
 		token->tok = INTEGER;
 		*source += imgLen;
-	} else if ((imgLen = matches_float(*source, &tmp)))
+	} else if ((imgLen = matches_float(*source, &tmp))) {
 		__set_nlen_tok(source, imgLen, FLOAT);
-	else {
+	} else {
 		fprintf(stderr, "error: invalid token \"%s\"\n", *source);
 		return NULL;
 	} return token;
