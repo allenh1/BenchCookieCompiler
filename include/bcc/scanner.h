e8fdefb3ac7b56cac14cf0065013220a7d75e4e4
Hunter L. Allen <hunter@openrobotics.org>
2018-01-31 22:13:35 -0500

    Linted headers. My bad.

--- include/bcc/scanner.h
+++ include/bcc/scanner.h
@@ -33,16 +33,16 @@ char * strdup(const char * str);
 /* macro to set the token to a given char */
 #define __set_char_tok(str, t)                          \
 	do {                                            \
-		token->image = strndup((*str)++, 1);	\
-		token->tok   = t;                       \
+		token->image = strndup((*str)++, 1);    \
+		token->tok = t;                       \
 	} while (0);
 
 /* like the above, but eat n chars */
 #define __set_nlen_tok(str, n, t)                       \
 	do {                                            \
-                token->image = strndup((*str), n);	\
-                token->tok   = t;                       \
-                *str += n;                              \
+		token->image = strndup((*str), n);      \
+		token->tok = t;                       \
+		*str += n;                              \
 	} while (0);
 
 #ifdef DEBUG
